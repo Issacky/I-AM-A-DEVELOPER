7aa2e29c509382f3b80b046d0743b8387696d02a
Oscar <oscar.ramoschacon01@utrgv.edu>
2021-01-08 22:48:20 -0600

    Initial Commit

---
+++ I AM A DEVELOPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM A DEVELOPER
+//
+//  Created by Oscar Ramos Chacon on 1/8/21.
+//  Copyright © 2021 Issack Shama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
